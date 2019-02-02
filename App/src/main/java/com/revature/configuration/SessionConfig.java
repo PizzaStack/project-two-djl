4d0fdc0465d1826711c11f7068676a6b461164eb
John Masiello <johnmasiello@github.com>
2019-02-02 15:36:53 -0600

    Repackaged Exception in their own package. Added useful reference links in the comments

--- App/src/main/java/com/revature/configuration/SessionConfig.java
+++ App/src/main/java/com/revature/configuration/SessionConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.revature.entity.MapUsers;
 
+// https://www.baeldung.com/spring-bean-scopes
+// https://stackoverflow.com/questions/32551077/how-does-the-session-scope-of-a-bean-work-in-a-spring-mvc-application
+// https://www.baeldung.com/spring-inject-prototype-bean-into-singleton
+// https://www.baeldung.com/spring-mvc-session-attributes
 @Configuration
 public class SessionConfig {
 	@Bean
--- App/src/main/java/com/revature/exception/AdmininstratorAccessNotGrantedException.java
+++ App/src/main/java/com/revature/exception/AdmininstratorAccessNotGrantedException.java
@@ -1,8 +1,10 @@
-package com.revature.controller;
+package com.revature.exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
+// https://www.baeldung.com/spring-response-status
+// https://www.baeldung.com/spring-boot-custom-error-page
 @ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "Admin Access Not Granted")
 public class AdmininstratorAccessNotGrantedException extends Exception {
 	private static final long serialVersionUID = 1L;
